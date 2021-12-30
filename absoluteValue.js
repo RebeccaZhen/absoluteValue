012c5ab6c37be19ee8306759678a1ea70633981f
RebeccaZhen <348628132@gapps.yrdsb.ca>
2021-12-29 23:55:42 -0500

    added absolute value in javascript

---
+++ absoluteValue.js
@@ -0,0 +1,18 @@
+function absoluteValue(let input) {
+	if (input >= 0) {
+		return input;
+	} else {
+		return 0 - input;
+	}
+}
+
+let Integer;
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+readline.question('Enter your integer: ', Integer => {
+	console.log ('${absoluteValue(Integer)}');
+	readline.close();
+});
