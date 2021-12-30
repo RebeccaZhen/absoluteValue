538d35fe81ffa186e643b722c8cccc04b0ffc865
RebeccaZhen <348628132@gapps.yrdsb.ca>
2021-12-29 22:10:58 -0500

    first commit

---
+++ absoluteValue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Enter your integer: ";
+	int Integer;
+	std::cin >> Integer;
+	std::cout << absoluteValue(Integer);
+	return 0;
+}
+
+int absoluteValue(int input) {
+	if (input >= 0) {
+		return input;
+	} else {
+		return 0 - input;
+	}
+}
